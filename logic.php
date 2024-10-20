9b1c20a0751b563993c892d2a29b9450415657fb
Antoine B <antoine.dropbox@gmail.com>
2024-10-20 11:03:25 +0200

    deuxieme comit

--- logic.php
+++ logic.php
@@ -23,12 +23,39 @@ $countries = [
 // Sélectionner un pays aléatoirement
 $country = array_rand($countries);
 
+
+
+//    $correctCapital = in_array($userInput, $countries[$country]);
+//    header("Location: index.php?country=$country&correct=$correctCapital");
+
+
+
 // Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $userInput = trim($_POST["capital"]);
     $userInput = strtolower($userInput);
-    $correctCapital = in_array($userInput, $countries[$country]);
-    header("Location: index.php?country=$country&correct=$correctCapital");
+    $selectedCountry = $_POST["country"];
+    $correctCapitals = $countries[$selectedCountry]; // Récupérer les deux versions des capitales
+
+    // Initialiser la variable de pourcentage de similarité
+    $maxPercent = 0;
+
+    // Vérifier la similarité avec les deux versions des capitales
+    foreach ($correctCapitals as $correctCapital) {
+        similar_text($userInput, $correctCapital, $percent);
+        $percent = round($percent, 1);
+        if ($percent > $maxPercent) {
+            $maxPercent = $percent;
+        }
+    }
+
+    // Vérifier si la similarité maximale est supérieure à 80%
+    if ($maxPercent > 80) {
+        $message = "Correct !";
+    } else {
+        $message = "Votre réponse $userInput est Incorrecte ($maxPercent %).<br>La capitale de $selectedCountry est " . implode(" ou ", $correctCapitals) . ".";
+    }
     exit();
 }
+
 ?>
